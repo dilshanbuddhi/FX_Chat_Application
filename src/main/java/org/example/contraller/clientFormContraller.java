4e3627a69ec4b78f7c19252b08ecacd1edce563a
dilshanbuddhi <dilshanbuddhi40.com>
2025-01-27 15:25:00 +0530

    first commit

--- src/main/java/org/example/contraller/clientFormContraller.java
+++ src/main/java/org/example/contraller/clientFormContraller.java
@@ -4,10 +4,10 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class clientFormContraller {
@@ -17,7 +17,7 @@ public class clientFormContraller {
     Socket socket;
     DataInputStream dataInputStream;
     DataOutputStream dataOutputStream;
-    String msg="";
+    String msg = "";
 
     public void initialize() {
         new Thread(() -> {
@@ -28,7 +28,6 @@ public class clientFormContraller {
                 clTxtArea.appendText("Client started. \n");
 
                 while (!socket.isClosed()) {
-
                     msg = dataInputStream.readUTF();
                     clTxtArea.appendText("Server: " + msg + "\n");
                 }
@@ -39,12 +38,51 @@ public class clientFormContraller {
         }).start();
     }
 
+    // Sending a text message
     public void sentOnAction(ActionEvent actionEvent) throws IOException {
         String text = cltextField.getText();
-            clTxtArea.appendText("you: " + text + "\n");
-            dataOutputStream.writeUTF(text);
-            dataOutputStream.flush();
-            cltextField.clear();
+        clTxtArea.appendText("You: " + text + "\n");
+
+        // Specify the message type as "TEXT"
+        dataOutputStream.writeUTF("TEXT");
+        dataOutputStream.writeUTF(text);
+        dataOutputStream.flush();
+        cltextField.clear();
+    }
+
+    // Select and send image
+    public void selectAndSendImage(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+        );
+
+        // Show the dialog and get the selected file
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        if (selectedFile != null) {
+            clTxtArea.appendText("Selected image: " + selectedFile.getName() + "\n");
+            try {
+                sendImage(selectedFile); // Send the selected image
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    // Send image file method
+    public void sendImage(File imageFile) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(imageFile);
+        byte[] imageBytes = new byte[(int) imageFile.length()];
+        fileInputStream.read(imageBytes);
+        fileInputStream.close();
+
+        // Specify the message type as "IMAGE"
+        dataOutputStream.writeUTF("IMAGE");
+        dataOutputStream.writeInt(imageBytes.length); // Send the length of the image data
+        dataOutputStream.write(imageBytes);           // Send the image bytes
+        dataOutputStream.flush();
+        clTxtArea.appendText("Image sent!\n");
+    }
+}
--- src/main/java/org/example/contraller/serverFormContraller.java
+++ src/main/java/org/example/contraller/serverFormContraller.java
@@ -4,16 +4,17 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class serverFormContraller {
     public TextArea ctxtArea;
     public TextField cTxtfield;
+    public ImageView imageView;
 
     ServerSocket serverSocket;
     Socket socket;
@@ -25,14 +26,25 @@ public class serverFormContraller {
         new Thread(() -> {
             try {
                 serverSocket = new ServerSocket(3000);
-                ctxtArea.appendText("Server started. \n");
+                Platform.runLater(() -> ctxtArea.appendText("Server started. \n"));
                 socket = serverSocket.accept();
                 dataInputStream = new DataInputStream(socket.getInputStream());
                 dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
                 while (!socket.isClosed()) {
-                    msg = dataInputStream.readUTF();
-                    ctxtArea.appendText("Client: " + msg + "\n");
+                    String messageType = dataInputStream.readUTF();
+
+                    if (messageType.equals("TEXT")) {
+                        ctxtArea.appendText("Client: " + msg + "\n");
+                    } else if (messageType.equals("IMAGE")) {
+                        int length = dataInputStream.readInt();  // Read the image length
+                        byte[] imageBytes = new byte[length];
+                        dataInputStream.readFully(imageBytes);  // Read the full image bytes
+                        Image image = new Image(new ByteArrayInputStream(imageBytes));
+                        Platform.runLater(() -> imageView.setImage(image));
+                    } else {
+                        Platform.runLater(() -> ctxtArea.appendText("Unknown message type received.\n"));
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -44,9 +56,9 @@ public class serverFormContraller {
         String message = cTxtfield.getText();
         ctxtArea.appendText("you: " + message + "\n");
 
+        dataOutputStream.writeUTF("TEXT");  // Specify that it's a text message
         dataOutputStream.writeUTF(message);
-            dataOutputStream.flush();
-            cTxtfield.clear();
-
+        dataOutputStream.flush();
+        cTxtfield.clear();
     }
 }
